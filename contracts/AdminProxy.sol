f30e57d649e147a32d2c111b1fefcecf997d01d1
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2023-08-03 10:01:36 -0300

    conserto

--- contracts/AdminProxy.sol
+++ contracts/AdminProxy.sol
@@ -2,4 +2,5 @@ pragma solidity 0.5.9;
 
 interface AdminProxy {
     function isAuthorized(address source) external view returns (bool);
+    function getAdminSize() external view returns (uint256);
 }
