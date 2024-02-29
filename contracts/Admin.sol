cb84e76cf6fdc8334433a5b98ea3ddc3e2be24a3
Adriano Gomes <adriano.gomes@bndes.gov.br>
2024-02-29 18:11:03 -0300

    exercicio 1 - sem copilot

--- contracts/Admin.sol
+++ contracts/Admin.sol
@@ -2,9 +2,13 @@ pragma solidity 0.5.9;
 
 import "./AdminProxy.sol";
 import "./AdminList.sol";
+import "truffle/Console.sol";
 
 
 contract Admin is AdminProxy, AdminList {
+    
+    mapping (address => uint) private quarentena;
+
     modifier onlyAdmin() {
         require(isAuthorized(msg.sender), "Sender not authorized");
         _;
@@ -24,19 +28,44 @@ contract Admin is AdminProxy, AdminList {
     }
 
     function addAdmin(address _address) public onlyAdmin returns (bool) {
+        //console.log("add");
+        //console.log(_address);
+        bool senderQuarentena = (quarentena[msg.sender] != 0 && 
+            ( block.timestamp < (quarentena[msg.sender]) ));
+        require(!senderQuarentena, "Sender quarentine");
+
         if (msg.sender == _address) {
             emit AdminAdded(false, _address, msg.sender, block.timestamp, "Adding own account as Admin is not permitted");
             return false;
         } else {
             bool result = add(_address);
             string memory message = result ? "Admin account added successfully" : "Account is already an Admin";
+            if(result){
+                //adicionar admin adicionado e emissor em quarentena
+                uint limiteQuarentena = block.timestamp + 1 days; 
+                quarentena[_address] = limiteQuarentena;
+                quarentena[msg.sender] = limiteQuarentena;
+                console.log(limiteQuarentena);
+            }
             emit AdminAdded(result, _address, msg.sender, block.timestamp, message);
             return result;
         }
     }
 
     function removeAdmin(address _address) public onlyAdmin notSelf(_address) returns (bool) {
+        //console.log("remove");
+        //console.log(msg.sender);
+        //console.log(_address);
+        bool senderQuarentena = (quarentena[msg.sender] != 0 && 
+            ( block.timestamp < (quarentena[msg.sender]) ));
+        require(!senderQuarentena, "Sender quarentine");
         bool removed = remove(_address);
+        if(removed){
+            //adicionar sender em quarentena
+            uint limiteQuarentena = block.timestamp + 1 days; 
+            quarentena[msg.sender] = limiteQuarentena;
+            console.log(limiteQuarentena);
+        }
         emit AdminRemoved(removed, _address, msg.sender, block.timestamp);
         return removed;
     }
