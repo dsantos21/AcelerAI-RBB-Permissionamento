9344c111fe62639440ef8b2d80b74e74d5924dab
Renato de Souza Mendes <renato.mendes@bndes.gov.br>
2024-02-29 15:54:40 -0300

    Fix bug in login functionality

--- contracts/Admin.sol
+++ contracts/Admin.sol
@@ -1,3 +1,4 @@
+
 pragma solidity 0.5.9;
 
 import "./AdminProxy.sol";
@@ -5,6 +6,8 @@ import "./AdminList.sol";
 
 
 contract Admin is AdminProxy, AdminList {
+    mapping(address => uint256) private lastActionTimestamp;
+
     modifier onlyAdmin() {
         require(isAuthorized(msg.sender), "Sender not authorized");
         _;
@@ -24,19 +27,34 @@ contract Admin is AdminProxy, AdminList {
     }
 
     function addAdmin(address _address) public onlyAdmin returns (bool) {
+        require(now - lastActionTimestamp[msg.sender] >= 1 days, "You can only perform this action once a day");
+        lastActionTimestamp[msg.sender] = now;
+
         if (msg.sender == _address) {
             emit AdminAdded(false, _address, msg.sender, block.timestamp, "Adding own account as Admin is not permitted");
             return false;
         } else {
             bool result = add(_address);
             string memory message = result ? "Admin account added successfully" : "Account is already an Admin";
+            
+            
+            lastActionTimestamp[msg.sender] = now;
+            lastActionTimestamp[_address] = now;
+
             emit AdminAdded(result, _address, msg.sender, block.timestamp, message);
             return result;
         }
     }
 
     function removeAdmin(address _address) public onlyAdmin notSelf(_address) returns (bool) {
+        require(now - lastActionTimestamp[msg.sender] >= 1 days, "You can only perform this action once a day");
+        lastActionTimestamp[msg.sender] = now;
+
         bool removed = remove(_address);
+
+        lastActionTimestamp[msg.sender] = now;
+        lastActionTimestamp[_address] = now;
+
         emit AdminRemoved(removed, _address, msg.sender, block.timestamp);
         return removed;
     }
