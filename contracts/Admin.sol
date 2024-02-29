fc147c4b8dc90954364aae851ef29a55b6c8e512
Eyder Padoan <eyder.santanna@bndes.gov.br>
2024-02-29 15:51:33 -0300

    Exercício 1 - Quarentena para inclusão e remoção de admins

--- contracts/Admin.sol
+++ contracts/Admin.sol
@@ -5,6 +5,9 @@ import "./AdminList.sol";
 
 
 contract Admin is AdminProxy, AdminList {
+    
+    mapping (address => uint256) private quarantineEndsAt; //1 based indexing. 0 means non-existent
+
     modifier onlyAdmin() {
         require(isAuthorized(msg.sender), "Sender not authorized");
         _;
@@ -27,8 +30,15 @@ contract Admin is AdminProxy, AdminList {
         if (msg.sender == _address) {
             emit AdminAdded(false, _address, msg.sender, block.timestamp, "Adding own account as Admin is not permitted");
             return false;
+        } else if (isInQuarantine(msg.sender)) {
+            emit AdminAdded(false, _address, msg.sender, block.timestamp, "Caller in quarantine. Adding accounts as Admin is not permitted");
+            revert();
         } else {
             bool result = add(_address);
+            if (result) {
+                putInQuarantine(msg.sender);
+                putInQuarantine(_address);
+            }
             string memory message = result ? "Admin account added successfully" : "Account is already an Admin";
             emit AdminAdded(result, _address, msg.sender, block.timestamp, message);
             return result;
@@ -36,9 +46,17 @@ contract Admin is AdminProxy, AdminList {
     }
 
     function removeAdmin(address _address) public onlyAdmin notSelf(_address) returns (bool) {
-        bool removed = remove(_address);
-        emit AdminRemoved(removed, _address, msg.sender, block.timestamp);
-        return removed;
+        if (isInQuarantine(msg.sender)) {
+            emit AdminAdded(false, _address, msg.sender, block.timestamp, "Caller in quarantine. Removing Admin accounts is not permitted");
+            revert();
+        } else {
+            bool removed = remove(_address);
+            if (removed) {
+                putInQuarantine(msg.sender);
+            }
+            emit AdminRemoved(removed, _address, msg.sender, block.timestamp);
+            return removed;
+        }
     }
 
     function getAdmins() public view returns (address[] memory){
@@ -47,5 +65,13 @@ contract Admin is AdminProxy, AdminList {
 
     function addAdmins(address[] memory accounts) public onlyAdmin returns (bool) {
         return addAll(accounts, msg.sender);
+    }
+
+    function isInQuarantine(address _account) internal view returns (bool) {
+        return quarantineEndsAt[_account] != 0 && quarantineEndsAt[_account] > now ;
+    }
+
+    function putInQuarantine(address _account) internal returns (bool) {
+        quarantineEndsAt[_account] = now + 1 days;
     }
 }
